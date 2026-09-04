5153a9873c4c2a723453f1f9ebc5e4674057b8e1
Seemore0 <walusimbi0752@gmail.com>
2026-09-04 22:47:40 +0700

    Add Capacitor Android configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ug.co.terrazzo.quotation",
+  appName: "Terrazzo Quotation Pro",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
